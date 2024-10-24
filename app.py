aeb600f1679bbd6258a9327956e34f79827fed17
wellionx <wellionx@foxmail.com>
2024-10-24 15:05:18 +0800

    comment metashape module

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from resources.login_resource import login_bp
-from resources.metashape_resource import metashape_bp
+#from resources.metashape_resource import metashape_bp
 from resources.plot_seg_resource import plot_seg_bp  # 导入新的蓝图
 from resources.plot_image_mask_resource import plot_image_mask_bp  # 导入新的图像掩膜蓝图
 from resources.image_predict_resource import image_predict_bp  # 导入图像推理蓝图
@@ -16,7 +16,7 @@ jwt = JWTManager(app)
 
 # 注册蓝图
 app.register_blueprint(login_bp)  # 注册登录蓝图
-app.register_blueprint(metashape_bp)  # 注册拼图蓝图
+#app.register_blueprint(metashape_bp)  # 注册拼图蓝图
 app.register_blueprint(plot_seg_bp)  # 注册网格绘制蓝图
 app.register_blueprint(plot_image_mask_bp)  # 注册图像掩膜蓝图
 app.register_blueprint(image_predict_bp)  # 注册图像推理蓝图
